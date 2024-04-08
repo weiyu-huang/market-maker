7ec2a49b28bfd6c5d350a088fb88be6da96b7ffc
weiyu-huang <weiyu_huang@icloud.com>
2024-04-07 19:52:49 -0700

    Round prices to int to improve robustness; basic limit order placement

--- src/naive.py
+++ src/naive.py
@@ -14,20 +14,25 @@ def main():
         'asks_distance_0',
         'asks_notional_0'
     ]]
-    df2.iloc[:, 'bid0'] = df2.midpoint * (1 + df2.bids_distance_0)
-    df2.iloc[:, 'ask0'] = df2.midpoint * (1 + df2.asks_distance_0)
+    df2['bid0'] = round(df2.midpoint * (1 + df2.bids_distance_0), 2)
+    df2['ask0'] = round(df2.midpoint * (1 + df2.asks_distance_0), 2)
 
     initial_cash = 10000
+    delta = 20
+    quantity = 0.01
+    target = 0.1
     exchange = Exchange(initial_cash=initial_cash)
-    exchange.place_order('BUY', 56000, 0.01)
-    exchange.place_order('BUY', 58000, 0.01)
-    exchange.place_order('SELL', 60000, 0.01)
-    exchange.place_order('SELL', 62000, 0.01)
 
     assets, cashes = [], []
     for row in df2.itertuples(index=False):
-        bid0, ask0 = getattr(row, 'bid0'), getattr(row, 'ask0')
-        asset, cash = exchange.simulate_latest_data(bid0, ask0)
+        time = getattr(row, 'system_time')
+        bid0, ask0, mid_price = getattr(row, 'bid0'), getattr(row, 'ask0'), getattr(row, 'midpoint')
+
+        if exchange.asset < target:
+            exchange.place_order('BUY', mid_price - delta, quantity)
+        else:
+            exchange.place_order('SELL', mid_price + delta, quantity)
+        asset, cash = exchange.simulate_latest_data(time, bid0, ask0)
         assets.append(asset)
         cashes.append(cash)
 
@@ -36,12 +41,12 @@ def main():
     df_r['price'] = df2['midpoint']
     plt.figure(figsize=(10, 10))
     plt.subplot(3, 1, 1)
-    plt.plot(df_r['system_time'], df_r['price'])
+    plt.plot( df_r['price'])
     plt.subplot(3, 1, 2)
-    plt.plot(df_r['system_time'], df_r['asset'])
+    plt.plot(df_r['asset'])
     plt.subplot(3, 1, 3)
-    plt.plot(df_r['system_time'], df_r['asset'] * df_r['price'] + df_r['cash'] - initial_cash)
-    plt.show()
+    plt.plot(df_r['asset'] * df_r['price'] + df_r['cash'] - initial_cash)
+    plt.savefig('temp.png', format='png')
 
 
 if __name__ == "__main__":
--- src/util_exchange.py
+++ src/util_exchange.py
@@ -1,5 +1,6 @@
 from heapq import heappush, heappop
 
+
 class Exchange:
     def __init__(self, initial_cash=10000):
         self.sell_prices = []  # minHeap
@@ -7,24 +8,31 @@ class Exchange:
         self.sell_quantities = {}  # sellPrice : sellQuantity
         self.buy_quantities = {}  # buyPrice : buyQuantity
         self.cash = initial_cash
+        self.initial_cash = initial_cash
         self.asset = 0
+        self.index = 0
+        self.mid_price = None
 
     def place_order(self, direction, price, quantity):
+        price = int(round(price, 2) * 100)
         if direction == 'BUY':
-            heappush(self.buy_prices, -price)
             if price in self.buy_quantities:
                 self.buy_quantities[price] += quantity
             else:
                 self.buy_quantities[price] = quantity
+                heappush(self.buy_prices, -price)
         elif direction == 'SELL':
-            heappush(self.sell_prices, price)
             if price in self.sell_quantities:
                 self.sell_quantities[price] += quantity
             else:
                 self.sell_quantities[price] = quantity
+                heappush(self.sell_prices, price)
 
-    def simulate_latest_data(self, bid0, ask0):
+    def simulate_latest_data(self, time, bid0, ask0):
         # TODO: need to check if we have enough bid or ask volumes
+        bid0, ask0 = int(round(bid0, 2) * 100), int(round(ask0, 2) * 100)
+        self.mid_price = (bid0 + ask0) // 2
+        self.index += 1
         self.simulate_buy(ask0)
         self.simulate_sell(bid0)
         return self.asset, self.cash
@@ -35,13 +43,14 @@ class Exchange:
         our_highest_buy_price = -self.buy_prices[0]
         if ask0 > our_highest_buy_price:  # lowest sell price is above our highest buy price
             return
-        transaction_amount = self.buy_quantities[our_highest_buy_price] * ask0
+        quantity = self.buy_quantities[our_highest_buy_price]
+        transaction_amount = quantity * ask0 / 100
         if self.cash < transaction_amount:  # not enough cash
             return
 
-        self.asset += self.buy_quantities[our_highest_buy_price]
+        self.asset += quantity
         self.cash -= transaction_amount
-        print(f'Bought {self.buy_quantities[our_highest_buy_price]} at {ask0}')
+        print(f'{self.index}: Bought {quantity} at {ask0 / 100:.2f}; Profit {self.compute_profit():.2f}')
         del self.buy_quantities[our_highest_buy_price]
         heappop(self.buy_prices)
 
@@ -51,11 +60,18 @@ class Exchange:
         our_lowest_sell_price = self.sell_prices[0]
         if bid0 < our_lowest_sell_price:  # highest buy price is below our lowest sell price
             return
-        if self.asset < self.sell_quantities[our_lowest_sell_price]:  # not enough asset
+        quantity = self.sell_quantities[our_lowest_sell_price]
+        if self.asset < quantity:  # not enough asset
             return
 
-        self.asset -= self.sell_quantities[our_lowest_sell_price]
-        self.cash += self.sell_quantities[our_lowest_sell_price] * bid0
-        print(f'Sold {self.sell_quantities[our_lowest_sell_price]} at {bid0}')
+        self.asset -= quantity
+        self.cash += quantity * bid0 / 100
+        print(f'{self.index}: Sold {quantity} at {bid0 / 100:.2f}; Profit {self.compute_profit():.2f}')
         del self.sell_quantities[our_lowest_sell_price]
         heappop(self.sell_prices)
+
+    def compute_profit(self):
+        return self.asset * self.mid_price / 100 + self.cash - self.initial_cash
+
+    def get_asset_amount(self):
+        return self.asset
