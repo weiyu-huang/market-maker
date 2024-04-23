9e462b40209ab7643d5de32ca0997e865e1863fe
weiyu-huang <weiyu_huang@icloud.com>
2024-04-22 23:03:24 -0700

    Implement basic Avellaneda Stoikov market making

--- src/naive.py
+++ src/naive.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
 from util_exchange import Exchange
+import warnings
+
+warnings.filterwarnings('ignore')
 
 
 def main():
@@ -14,39 +17,79 @@ def main():
         'asks_distance_0',
         'asks_notional_0'
     ]]
-    df2['bid0'] = round(df2.midpoint * (1 + df2.bids_distance_0), 2)
-    df2['ask0'] = round(df2.midpoint * (1 + df2.asks_distance_0), 2)
+    df2.loc[:, 'bid0'] = round(df2.midpoint * (1 + df2.bids_distance_0), 2)
+    df2.loc[:, 'ask0'] = round(df2.midpoint * (1 + df2.asks_distance_0), 2)
+    df2['bid0_std'] = df2.bid0.rolling(window=20).std()
+    df2['ask0_std'] = df2.ask0.rolling(window=20).std()
+    df2.loc[:, 'std'] = (df2.bid0_std + df2.ask0_std) / 2
+    df2.loc[:, 'volume'] = (df2.bids_notional_0 + df2.asks_notional_0) / 2
 
     initial_cash = 100000
-    delta = 40
-    quantity = 0.05
-    target = 0.1
-    exchange = Exchange(initial_cash=initial_cash)
+    target_asset = 0.1
+    gamma = 0.01
+    t = 0
+    T = df2.shape[0]
+    exchange = Exchange(initial_cash=initial_cash, max_open_orders=3)
 
-    assets, cashes = [], []
-    exchange.place_order('BUY', 56000, 0.15)
-    exchange.place_order('SELL', 60000, 0.05)
-    exchange.place_order('SELL', 61000, 0.05)
-    exchange.place_order('SELL', 62000, 0.05)
+    assets, cashes, rs, optimal_buys, optimal_sells = [], [], [], [], []
     for row in df2.itertuples(index=False):
-        time = getattr(row, 'system_time')
-        bid0, ask0, mid_price = getattr(row, 'bid0'), getattr(row, 'ask0'), getattr(row, 'midpoint')
+        t += 1
+        bid0, ask0, s = row.bid0, row.ask0, row.midpoint
+        vol, std = row.volume, row.std
 
-        # if exchange.asset < target:
-        #     exchange.place_order('BUY', mid_price - delta, quantity)
-        # else:
-        #     exchange.place_order('SELL', mid_price + delta, quantity)
+        if pd.isna(std):  # wait until we have enough volatility data
+            assets.append(np.nan)
+            cashes.append(np.nan)
+            rs.append(np.nan)
+            optimal_buys.append(np.nan)
+            optimal_sells.append(np.nan)
+            continue
 
-        asset, cash = exchange.simulate_latest_data(time, bid0, ask0)
+        # if t > 10000:
+        #     break
+
+        q = target_asset - exchange.get_asset_amount()
+        # T_minus_t = (T - t) / T  # normalized to 1
+        T_minus_t = 1
+        r = s - q * gamma * std * std * T_minus_t  # reserve price
+        kappa = vol / 500_000
+        delta_a_plus_delta_b = gamma * std * std * T_minus_t + 2 / gamma * np.log(1 + gamma / kappa)
+        delta = delta_a_plus_delta_b / 2
+
+        alpha = 0.5
+        min_quantity = 0.1
+        if exchange.asset < 0.5:
+            exchange.place_order('BUY', r - delta, max(q * alpha, min_quantity))
+        exchange.place_order('SELL', r + delta, max(-q * alpha, min_quantity))
+
+        if exchange.buy_orders and exchange.sell_orders:
+            print(f"Index {t}: bid0: {bid0}, ask0: {ask0}, highestBuy: {max(exchange.buy_orders)[0]}, lowestSell: {min(exchange.sell_orders)[0]}")
+        asset, cash = exchange.simulate_latest_data(bid0, ask0)
         assets.append(asset)
         cashes.append(cash)
+        rs.append(r)
+        optimal_buys.append(r - delta)
+        optimal_sells.append(r + delta)
 
-    df_r = pd.DataFrame({'asset': assets, 'cash': cashes})
+    df_r = pd.DataFrame({
+        'asset': assets,
+        'cash': cashes,
+        'reserve_price': rs,
+        'optimal_buys': optimal_buys,
+        'optimal_sells': optimal_sells
+    })
     df_r['system_time'] = df2['system_time']
     df_r['price'] = df2['midpoint']
-    plt.figure(figsize=(10, 10))
+    df_r['bid0'] = df2['bid0']
+    df_r['ask0'] = df2['ask0']
+    df_r = df_r.iloc[:, :]
+    plt.figure(figsize=(15, 15))
     plt.subplot(3, 1, 1)
-    plt.plot( df_r['price'])
+    plt.plot(df_r['bid0'])
+    plt.plot(df_r['ask0'])
+    plt.plot(df_r['reserve_price'])
+    plt.legend(['bid', 'ask', 'r'], loc='best')
+
     plt.subplot(3, 1, 2)
     plt.plot(df_r['asset'])
     plt.subplot(3, 1, 3)
--- src/util_exchange.py
+++ src/util_exchange.py
@@ -1,9 +1,9 @@
-from sortedcontainers import SortedDict
+from collections import deque
 
 class Exchange:
     def __init__(self, initial_cash=10000, max_open_orders=5):
-        self.sell_orders = SortedDict()  # sellPrice : sellQuantity
-        self.buy_orders = SortedDict()  # buyPrice : buyQuantity
+        self.sell_orders = deque()  # (sell_price, sell_quantity)
+        self.buy_orders = deque()  # (buy_price, buy_quantity)
         self.cash = initial_cash
         self.initial_cash = initial_cash
         self.max_open_orders = max_open_orders
@@ -12,65 +12,57 @@ class Exchange:
         self.mid_price = None
 
     def place_order(self, direction, price, quantity):
-        price = int(round(price, 2) * 100)
-        quantity = (round(quantity, 2) * 100)
+        price = round(price, 2)
         if direction == 'BUY':
-            if price in self.buy_orders:
-                self.buy_orders[price] += quantity
-            else:
-                self.buy_orders.setdefault(price, quantity)
-                if len(self.buy_orders) > self.max_open_orders:
-                    self.buy_orders.popitem(0)  # remove the cheapest
+            self.buy_orders.append((price, quantity))
+            if len(self.buy_orders) > self.max_open_orders:
+                self.buy_orders.popleft()
         elif direction == 'SELL':
-            if price in self.sell_orders:
-                self.sell_orders[price] += quantity
-            else:
-                self.sell_orders.setdefault(price, quantity)
-                if len(self.sell_orders) > self.max_open_orders:
-                    self.sell_orders.popitem(-1)  # remove the most expensive
+            self.sell_orders.append((price, quantity))
+            if len(self.sell_orders) > self.max_open_orders:
+                self.sell_orders.popleft()
 
-    def simulate_latest_data(self, time, bid0, ask0):
+    def simulate_latest_data(self, bid0, ask0):
         # TODO: need to check if we have enough bid or ask volumes
-        bid0, ask0 = int(round(bid0, 2) * 100), int(round(ask0, 2) * 100)
-        self.mid_price = (bid0 + ask0) // 2
+        self.mid_price = (bid0 + ask0) / 2
         self.index += 1
         self.simulate_buy(ask0)
         self.simulate_sell(bid0)
-        return self.asset / 100, self.cash
+        return self.asset, self.cash
 
     def simulate_buy(self, ask0):
-        if not self.buy_orders:  # no buy orders
+        if len(self.buy_orders) == 0:  # no buy orders
             return
-        our_highest_buy_price = self.buy_orders.keys()[-1]
-        if ask0 > our_highest_buy_price:  # lowest sell price is above our highest buy price
+        max_tuple = max(self.buy_orders)
+        (our_highest_buy_price, quantity) = max_tuple
+        if ask0 > our_highest_buy_price:  # make market when lowest sell price <= our highest buy price
             return
-        quantity = self.buy_orders[our_highest_buy_price]
-        transaction_amount = quantity * ask0 / 10000
+        transaction_amount = round(quantity * ask0, 2)
         if self.cash < transaction_amount:  # not enough cash
             return
 
         self.asset += quantity
         self.cash -= transaction_amount
-        print(f'{self.index}: Bought {quantity / 100} at {ask0 / 100:.2f}; Profit {self.compute_profit():.2f}')
-        self.buy_orders.pop(our_highest_buy_price)
+        print(f'{self.index}: Bought {quantity} at {ask0:.2f}; Profit {self.compute_profit():.2f}')
+        self.buy_orders.remove(max_tuple)
 
     def simulate_sell(self, bid0):
-        if not self.sell_orders:  # no sell orders
+        if len(self.sell_orders) == 0:  # no sell orders
             return
-        our_lowest_sell_price = self.sell_orders.keys()[0]
-        if bid0 < our_lowest_sell_price:  # highest buy price is below our lowest sell price
+        min_tuple = min(self.sell_orders)
+        (our_lowest_sell_price, quantity) = min_tuple
+        if bid0 < our_lowest_sell_price:  # make market when highest buy price >= our lowest sell price
             return
-        quantity = self.sell_orders[our_lowest_sell_price]
         if self.asset < quantity:  # not enough asset
             return
 
         self.asset -= quantity
-        self.cash += quantity * bid0 / 10000
-        print(f'{self.index}: Sold {quantity} at {bid0 / 100:.2f}; Profit {self.compute_profit():.2f}')
-        self.sell_orders.pop(our_lowest_sell_price)
+        self.cash += round(quantity * bid0, 2)
+        print(f'{self.index}: Sold {quantity} at {bid0:.2f}; Profit {self.compute_profit():.2f}')
+        self.sell_orders.remove(min_tuple)
 
     def compute_profit(self):
-        return self.asset / 100 * self.mid_price / 100 + self.cash - self.initial_cash
+        return self.asset * self.mid_price + self.cash - self.initial_cash
 
     def get_asset_amount(self):
-        return self.asset / 100
+        return self.asset
